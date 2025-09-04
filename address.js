4c99941a5cd6ec0dbbd0363c76c6155b06bf66bb
be7shishirp <be7shishirp@gmail.com>
2025-09-04 08:29:26 +0545

    Create address.js

---
+++ address.js
@@ -0,0 +1,7 @@
+// Map links: open target google maps with coordinates if you want; placeholders open google maps search
+document.querySelectorAll('.place-card .map-link').forEach(a=>{
+  // if the href is generic, replace with a better search using the place title
+  a.addEventListener('click', (e)=>{
+    // allow default; could enhance with coordinates mapping later
+  })
+});
